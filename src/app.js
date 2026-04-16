e9a869d2b3a5d039c93fc59b2b6621f7afea3438
sarva1210 <sarva.vish77@gmail.com>
2026-04-16 20:34:11 +0530

    app file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (_req, res) => {
+    res.status(200).json({ message: "Server is running" });
+});
 
 export default app;
