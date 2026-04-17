086bb2277f585733940aa45c49c9f0609b6c8275
sarva1210 <sarva.vish77@gmail.com>
2026-04-17 21:29:01 +0530

    ImageKit storage, validation, and seller authentication

---
+++ src/routes/product.routes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { authenticateSeller } from '../middlewares/auth.middleware.js';
+import { createProduct, getSellerProducts } from '../controllers/product.controller.js';
+import multer from "multer";
+import { createProductValidator } from '../validator/product.validator.js';
+
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5 MB
+    }
+})
+
+const router = express.Router();
+
+//POST /api/products  ;  Create a new product   ;   Private (Seller only)
+router.post("/", authenticateSeller, upload.array('images', 7), createProductValidator, createProduct)
+
+
+//GET /api/products/seller  ;    Get all products of the authenticated seller  ;   Private (Seller only)
+router.get("/seller", authenticateSeller, getSellerProducts)
+
+
+export default router;
