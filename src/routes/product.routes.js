d3a059b949d0d51f56f8f43b600962a3bdea48ef
sarva1210 <sarva.vish77@gmail.com>
2026-04-20 18:57:47 +0530

    updated routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authenticateSeller } from '../middlewares/auth.middleware.js';
-import { createProduct, getSellerProducts } from '../controllers/product.controller.js';
+import { createProduct, getSellerProducts, getAllProducts, getProductDetails, addProductVariant } from '../controllers/product.controller.js';
 import multer from "multer";
 import { createProductValidator } from '../validator/product.validator.js';
 
@@ -21,5 +21,13 @@ router.post("/", authenticateSeller, upload.array('images', 7), createProductVal
 //GET /api/products/seller  ;    Get all products of the authenticated seller  ;   Private (Seller only)
 router.get("/seller", authenticateSeller, getSellerProducts)
 
+// GET /api/products    ;     Get all products     ;     Public
+router.get("/", getAllProducts)
+
+//GET /api/products/detail/:id    ;    Get product details by ID   ;   Public
+router.get("/detail/:id", getProductDetails)
+
+//post /api/products/:productId/variants   ;   Add a new variant to a product   ;   Private (Seller only)
+router.post("/:productId/variants", authenticateSeller, upload.array('images', 7), addProductVariant)
 
 export default router;
