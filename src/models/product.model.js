ba1a3815083dad3ea68bc83b6ad1676f63b97cc8
sarva1210 <sarva.vish77@gmail.com>
2026-04-20 18:57:17 +0530

    updated models

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema({
     },
     seller: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',    
+        ref: 'user',
         required: true
     },
     price: {
@@ -32,8 +32,41 @@ const productSchema = new mongoose.Schema({
                 required: true
             }
         }
+    ],
+    variants: [
+        {
+            images: [
+                {
+                    url: {
+                        type: String,
+                        required: true
+                    }
+                }
+            ],
+            stock: {
+                type: Number,
+                default: 0
+            },
+            attributes: {
+                type: Map,
+                of: String
+            },
+            price: {
+                amount: {
+                    type: Number,
+                    required: true
+                },
+                currency: {
+                    type: String,
+                    enum: [ "USD", "EUR", "GBP", "JPY", "INR" ],
+                    default: "INR"
+                }
+            }
+        },
+
     ]
 }, { timestamps: true })
+
 
 const productModel = mongoose.model('product', productSchema);
 
