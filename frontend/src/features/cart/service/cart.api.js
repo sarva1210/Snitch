568a831545ccaba07ea1ae3070fcbe12861f476a
sarva1210 <sarva.vish77@gmail.com>
2026-04-23 21:40:24 +0530

    display price comparison and savings information

--- frontend/src/features/cart/service/cart.api.js
+++ frontend/src/features/cart/service/cart.api.js
@@ -14,6 +14,7 @@ export const addItem = async ({ productId, variantId }) => {
     return response.data
 }
 
+
 export const getCart = async () => {
     const response = await cartApiInstance.get("/")
     return response.data
