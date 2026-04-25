8760b89bd80b698d99a28af6aa47fa9939f4dcd4
sarva1210 <sarva.vish77@gmail.com>
2026-04-25 19:26:45 +0530

    cart

--- frontend/src/features/cart/service/cart.api.js
+++ frontend/src/features/cart/service/cart.api.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+
 const cartApiInstance = axios.create({
     baseURL: "/api/cart",
     withCredentials: true
@@ -14,8 +15,27 @@ export const addItem = async ({ productId, variantId }) => {
     return response.data
 }
 
-
 export const getCart = async () => {
     const response = await cartApiInstance.get("/")
+    return response.data
+}
+
+export const incrementCartItemApi = async ({ productId, variantId }) => {
+    const response = await cartApiInstance.patch(`/quantity/increment/${productId}/${variantId}`)
+    return response.data
+}
+
+export const createCartOrder = async () => {
+    const response = await cartApiInstance.post("/payment/create/order")
+    return response.data
+}
+
+export const verifyCartOrder = async ({ razorpay_order_id, razorpay_payment_id, razorpay_signature }) => {
+    const response = await cartApiInstance.post("/payment/verify/order", {
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature
+    })
+
     return response.data
 }
