ee4ab7740b1a775e921481e796edadcff99357ee
sarva1210 <sarva.vish77@gmail.com>
2026-04-27 20:36:33 +0530

    payment gateway

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { authenticateUser } from '../middlewares/auth.middleware.js';
-import { validateAddToCart } from '../validator/cart.validator.js';
-import { addToCart, getCart } from '../controllers/cart.controller.js';
+import { validateAddToCart, validateIncrementCartItemQuantity } from '../validator/cart.validator.js';
+import { addToCart, createOrderController, getCart, incrementCartItemQuantity, verifyOrderController } from '../controllers/cart.controller.js';
 
 const router = express.Router();
-
 
 /**
  * @route POST /api/cart/add/:productId/:variantId
@@ -17,12 +16,29 @@ const router = express.Router();
 router.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart)
 
 
-
 /**
  * @route GET /api/cart
  * @desc Get user's cart
  * @access Private
  */
 router.get('/', authenticateUser, getCart)
+
+
+/**
+ * @route PATCH /api/cart/quantity/increment/:productId/:variantId
+ * @desc Increment item quantity in cart by one
+ * @access Private
+ * @argument productId - ID of the product to update
+ * @argument variantId - ID of the variant to update
+ */
+router.patch("/quantity/increment/:productId/:variantId", authenticateUser, validateIncrementCartItemQuantity, incrementCartItemQuantity)
+
+
+/**
+ * @route POST /api/cart/payment/create/order
+ */
+router.post("/payment/create/order", authenticateUser, createOrderController)
+
+router.post("/payment/verify/order", authenticateUser, verifyOrderController)
 
 export default router;
