8aa05e3fcbbbe52c83cef818bad39f75f7417c98
sarva1210 <sarva.vish77@gmail.com>
2026-04-27 20:39:14 +0530

    payment gateway

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,8 +1,16 @@
 import cartModel from "../models/cart.model.js";
 import productModel from "../models/product.model.js";
 import { stockOfVariant } from "../dao/product.dao.js";
+import mongoose from "mongoose";
+import { createOrder } from "../services/payment.service.js";
+import { getCartDetails } from "../dao/cart.dao.js";
+import paymentModel from "../models/payment.model.js";
+import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils.js";
+import { config } from "../config/config.js";
+
 
 export const addToCart = async (req, res) => {
+
     const { productId, variantId } = req.params
     const { quantity = 1 } = req.body
 
@@ -71,7 +79,7 @@ export const addToCart = async (req, res) => {
 export const getCart = async (req, res) => {
     const user = req.user
 
-    let cart = await cartModel.findOne({ user: user._id }).populate("items.product")
+    let cart = await getCartDetails(user._id)
 
     if (!cart) {
         cart = await cartModel.create({ user: user._id })
@@ -81,5 +89,143 @@ export const getCart = async (req, res) => {
         message: "Cart fetched successfully",
         success: true,
         cart
+    })
+}
+
+export const incrementCartItemQuantity = async (req, res) => {
+    const { productId, variantId } = req.params
+
+    const product = await productModel.findOne({
+        _id: productId,
+        "variants._id": variantId
+    })
+
+    if (!product) {
+        return res.status(404).json({
+            message: "Product or variant not found",
+            success: false
+        })
+    }
+
+    const cart = await cartModel.findOne({ user: req.user._id })
+
+    if (!cart) {
+        return res.status(404).json({
+            message: "Cart not found",
+            success: false
+        })
+    }
+
+    const stock = await stockOfVariant(productId, variantId)
+
+    const itemQuantityInCart = cart.items.find(item => item.product.toString() === productId && item.variant?.toString() === variantId)?.quantity || 0
+
+    if (itemQuantityInCart + 1 > stock) {
+        return res.status(400).json({
+            message: `Only ${stock} items left in stock. and you already have ${itemQuantityInCart} items in your cart`,
+            success: false
+        })
+    }
+
+    await cartModel.findOneAndUpdate(
+        { user: req.user._id, "items.product": productId, "items.variant": variantId },
+        { $inc: { "items.$.quantity": 1 } },
+        { new: true }
+    )
+
+    return res.status(200).json({
+        message: "Cart item quantity incremented successfully",
+        success: true
+    })
+}
+
+export const createOrderController = async (req, res) => {
+
+
+    const cart = await getCartDetails(req.user._id)
+
+    if (!cart) {
+        return res.status(400).json({
+            message: "Cart is empty",
+            success: false
+        })
+    }
+
+    const order = await createOrder({ amount: cart.totalPrice, currency: cart.currency })
+
+    const payment = await paymentModel.create({
+        user: req.user._id,
+        razorpay: {
+            orderId: order.id,
+        },
+        price: {
+            amount: cart.totalPrice,
+            currency: cart.currency
+        },
+        orderItems: cart.items.map(item => ({
+            title: item.product.title,
+            productId: item.product._id,
+            variantId: item.variant,
+            quantity: item.quantity,
+            images: item.product.variants.images || item.product.images,
+            description: item.product.description,
+            price: {
+                amount: item.product.variants.price.amount || item.product.price.amount,
+                currency: item.product.variants.price.currency || item.product.price.currency
+            }
+        }))
+    })
+
+    return res.status(200).json({
+        message: "Order created successfully",
+        success: true,
+        order
+    })
+}
+
+export const verifyOrderController = async (req, res) => {
+    const {
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature
+    } = req.body
+
+    const payment = await paymentModel.findOne({
+        "razorpay.orderId": razorpay_order_id,
+        status: "pending"
+    })
+
+    if (!payment) {
+        return res.status(400).json({
+            message: "Payment not found",
+            success: false
+        })
+    }
+
+    const isPaymentValid = validatePaymentVerification({
+        order_id: razorpay_order_id,
+        payment_id: razorpay_payment_id,
+    }, razorpay_signature, config.RAZORPAY_KEY_SECRET)
+
+    if (!isPaymentValid) {
+        payment.status = "failed"
+        await payment.save()
+
+        return res.status(400).json({
+            message: "Payment verification failed",
+            success: false
+        })
+    }
+
+    payment.status = "paid"
+
+    payment.razorpay.paymentId = razorpay_payment_id
+    payment.razorpay.signature = razorpay_signature
+
+    await payment.save()
+
+    return res.status(200).json({
+        message: "Payment verified successfully",
+        success: true
     })
 }
